752baf0513b81882907cdf9c84c472f75873d97c
chavez <95760172+spumdonut@users.noreply.github.com>
2021-12-29 02:30:53 -0500

    1.3

--- canvas.tsx
+++ canvas.tsx
@@ -1,64 +1,67 @@
 import React, { useState } from "react"
 import '../App.css'
-import * as THREE from 'three';
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { Vector2, Vector3 } from "three"
 
-let scene: THREE.Scene
-let camera: THREE.PerspectiveCamera
-let renderer: THREE.WebGLRenderer
-let boxGeometry: THREE.BoxGeometry
-let material: THREE.MeshBasicMaterial
-let mesh: THREE.Mesh
+const init = () => {
+    // scene
+    let scene: THREE.Scene
+    scene = new THREE.Scene()
+
+    // camera
+    let camera: THREE.PerspectiveCamera
+    camera = new THREE.PerspectiveCamera(
+        75,
+        // eslint-disable-next-line no-restricted-globals
+        innerWidth / innerHeight,
+        0.1,
+        1000
+    )
+    camera.position.z = 5
+
+    // renderer
+    let renderer: THREE.WebGLRenderer
+    renderer = new THREE.WebGLRenderer()
+    // eslint-disable-next-line no-restricted-globals
+    renderer.setSize(innerWidth, innerHeight)
+
+    // box
+    let boxGeometry: THREE.BoxGeometry
+    boxGeometry = new THREE.BoxGeometry(
+        1, 1, 1
+    )
+
+    // material
+    let material: THREE.MeshBasicMaterial
+    material = new THREE.MeshBasicMaterial({
+        color: 0x00FF00
+    })
+
+    // mesh
+    let mesh: THREE.Mesh
+    mesh = new THREE.Mesh(
+        boxGeometry,
+        material
+    )
+    scene.add(mesh)
+
+    document.body.appendChild<HTMLCanvasElement>(renderer.domElement)
+
+    renderer.render(scene, camera)
+}
+init()
 
 let text: string
-
-scene = new THREE.Scene()
-
-camera = new THREE.PerspectiveCamera(
-     75,
-      // eslint-disable-next-line no-restricted-globals
-      innerWidth / innerHeight,
-     0.1,
-    1000
-)
-
-renderer = new THREE.WebGLRenderer()
-// eslint-disable-next-line no-restricted-globals
-renderer.setSize(innerWidth, innerHeight)
-
-boxGeometry = new THREE.BoxGeometry(
-    1, 1, 1
-)
-
-material = new THREE.MeshBasicMaterial({
-    color: 0x00FF00
-})
-
-mesh = new THREE.Mesh(
-    boxGeometry,
-    material
-)
-
-console.log(scene)
-console.log(camera)
-console.log(renderer)
-console.log(boxGeometry)
-console.log(material)
-console.log(mesh)
-
-scene.add(mesh)
-
-document.body.appendChild<HTMLCanvasElement>(renderer.domElement)
-
-camera.position.z = 5
-
-renderer.render(scene, camera)
-
-// react stuff
 
 export const Canvas: React.FC = () => {
 
     const [ changeText, setChangeText ] = useState<string>('')
     text = changeText
+
+    const setChangeTextFunction = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setChangeText(e.target.value)
+    }
     
     return (
         <>
@@ -66,8 +69,8 @@ export const Canvas: React.FC = () => {
                 {text}
             </div>
             <div className='input'>
-                <input onChange={(e: React.ChangeEvent<HTMLInputElement>) => { setChangeText(e.target.value); } }></input>
-                {/* <button onClick={}></button> */}
+                <input onChange={ setChangeTextFunction } />
+                {/* <button onClick={} /> */}
             </div>
         </>
     )
